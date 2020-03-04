b88830951ef98bc20bc0563eabd56bb8fc9dd15e
Nate <github@najo.com>
2020-03-04 11:51:28 -0600

    Update css.php from main project.

--- themes/default/css.php
+++ themes/default/css.php
@@ -1721,6 +1721,12 @@ $default_login = ($_REQUEST['login'] == 'default') ? true : false;
 		font-size: <?php echo ($_SESSION['theme']['form_table_row_text_size']['text'] != '') ? $_SESSION['theme']['form_table_row_text_size']['text'] : '9pt'; ?>;
 		}
 
+	.vtablerow > label {
+		margin-left: 0.6em;
+		margin-right: 0.6em;
+		margin-bottom: 2px;
+		}
+
 	.row_style0 {
 		border-bottom: 1px solid <?php echo ($_SESSION['theme']['table_row_border_color']['text'] != '') ? $_SESSION['theme']['table_row_border_color']['text'] : '#c5d1e5'; ?>;
 		background: <?php echo ($_SESSION['theme']['table_row_background_color_dark']['text'] != '') ? $_SESSION['theme']['table_row_background_color_dark']['text'] : '#e5e9f0'; ?>;
@@ -2485,6 +2491,22 @@ $default_login = ($_REQUEST['login'] == 'default') ? true : false;
 		white-space: nowrap;
 		}
 
+/* EDIT ********************************************************************************/
+
+	td.edit_delete_checkbox_all {
+		text-align: center;
+		width: 50px;
+		}
+
+	td.edit_delete_checkbox_all input[type=checkbox] {
+		vertical-align: middle;
+		margin-top: -2px;
+		}
+
+	td.edit_delete_checkbox_all > span:nth-child(2) {
+		display: none;
+		}
+
 /* CURSORS ***********************************************************************/
 
 	.cursor-default { cursor: default; }
@@ -2605,9 +2627,7 @@ $default_login = ($_REQUEST['login'] == 'default') ? true : false;
 
 	@media(min-width: 700px) {
 		.modal-window > div {
-			width: 40%;
-			min-width: 400px;
-			max-width: 500px;
+			width: 500px;
 			margin: 10% auto;
 			border-radius: 5px;
 			}
